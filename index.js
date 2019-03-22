4508dcd16c4202df307285c087efa2417d39127b
Abid Khalil <abzno1@gmail.com>
2019-03-22 23:21:26 +0000

    Initial code

---
+++ index.js
@@ -0,0 +1,8 @@
+import moment from 'moment';
+
+console.log("Hello from JavaScript!");
+console.log(moment().startOf('day').fromNow());
+
+var name = "Bob", time = "today";
+
+console.log(`Hello ${name}, how are you ${time}?`);
